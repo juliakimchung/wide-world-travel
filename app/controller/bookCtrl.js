f3dd53273fb8616225170187164e6d77a84211da
julia kim-chung <julhkm@gmail.com>
2016-11-30 20:32:58 -0600

    Add files via upload

---
+++ app/controller/bookCtrl.js
@@ -0,0 +1,33 @@
+"use strict";
+
+app.controller('bookCtrl' , function($scope){
+
+  $scope.bookItems = [];
+
+  $scope.loadBooks = () => {
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: '../../data/guides.json',
+        success: (result) => {
+        	console.log("result from promise",result );
+          resolve(result.guides);
+        },
+        error: (error) => {
+          console.log("something went wrong");
+          reject(error);
+        }
+      });
+    });
+  };
+
+  $scope.loadBooks()
+    .then((dataArrayFromPromise) => {
+    	console.log("dataArray from after loadBooks()",dataArrayFromPromise );
+      $scope.bookItems = dataArrayFromPromise;
+      $scope.$apply();
+    });
+
+ 	// $scope.welcome = "Welcome To Our Travel Guide";
+ 	// $scope.showListView = true;
+});
